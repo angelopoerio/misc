8567993fe2e7e63b8e770b9f37902440b85a7c38
angelopoerio <angelo.poerio@gmail.com>
2016-08-23 11:21:14 +0200

    fixed typo

--- useless_linux/build_initrd.sh
+++ useless_linux/build_initrd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir initramfs
 mkdir initramfs/{bin,etc,proc,sbin,sys,usr}
-cc init.c -static -o initramfs/init # statically linked so we do not have to care about dinamic libraries!
+cc init.c -static -o initramfs/init # statically linked so we do not have to care about dynamic libraries!
 cd initramfs
 find . -print0 \
     | cpio --null -ov --format=newc \
