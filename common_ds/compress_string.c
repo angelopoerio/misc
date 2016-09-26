febca1f95ae23d46fff474bf8087d4a3b46b1111
angelopoerio <angelo.poerio@gmail.com>
2016-09-26 23:28:58 +0200

    added more comments

--- common_ds/compress_string.c
+++ common_ds/compress_string.c
@@ -1,5 +1,10 @@
 /*
 	Uber simple string compression - Angelo Poerio <angelo.poerio@gmail.com>
+	Example: 
+	./compress_string aaabb gggggggggggggggggggggggggggggggggggg poirrrrrtttt
+	Original: aaabb | Compressed: a3b2 | Saved: 80.00 % 
+	Original: gggggggggggggggggggggggggggggggggggg | Compressed: g36 | Saved: 8.33 % 
+	Original: poirrrrrtttt | Compressed: poir5t4 | Saved: 58.33 % 
 */
 
 #include <stdio.h>
@@ -45,7 +50,7 @@ int main(int argc, char **argv) {
 		memset(compressed_string,0,or_len*2);
 		compress(argv[i],compressed_string);
 		com_len = strlen(compressed_string);
-		printf("Original: %s | Compressed: %s | Ratio: %.2f % \n", argv[i],
+		printf("Original: %s | Compressed: %s | Saved: %.2f % \n", argv[i],
 							compressed_string,
 							((double)com_len/(double)or_len) * 100.0);
 		free(compressed_string);
