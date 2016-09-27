b5067baf2d382290aec53caa0191adb5147f0466
angelopoerio <angelo.poerio@gmail.com>
2016-09-27 21:21:16 +0200

    added kernel_oop_finder

--- kernel_oop_finder/fabfile.py
+++ kernel_oop_finder/fabfile.py
@@ -8,7 +8,7 @@ PEM = 'key.pem' # change with your pem to access the systems
 def do_grep(s):
     env.key_filename=[PEM]
     with settings(warn_only=True):
-        run("dmesg|egrep -irn 'call trace' -A 10") # print 10 lines of call stack 
+        run("dmesg|egrep -i 'call trace' -A 10") # print 10 lines of call stack 
 
 @hosts('dummyhost1', 'dummyhost2') # changes to your needs
 @parallel
