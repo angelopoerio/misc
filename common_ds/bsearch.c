c0c66dc1be69c550ac1c85c01763f9cb3b821e44
angelopoerio <angelo.poerio@gmail.com>
2016-09-28 23:03:35 +0200

    added iterative bsearch

--- common_ds/bsearch.c
+++ common_ds/bsearch.c
@@ -3,6 +3,28 @@
   Binary search: Angelo Poerio <angelo.poerio@gmail.com>
 */
 
+/* iterative binary search */
+int bsearch_it(int *list, int start, int end, int x) {
+	int m;
+	while(start <= end) {
+		m = (start+end)/2;
+		if(list[m] == x)
+		{
+			return m;
+		}
+	
+		else if(list[m] < x) 
+		{
+			start = m + 1;
+		}
+
+		else {
+			end = m - 1;
+		}
+	}
+	return -1;
+}
+
 /* recursive binary search */
 int bsearch(int *list, int start, int end, int x) {
 	int middle = (start + end) / 2;
@@ -27,5 +49,6 @@ int bsearch(int *list, int start, int end, int x) {
 
 int main(int argc, char *argv[]) {
 	int lst[] = {1,2,3,4,5,6,7,8,9,10};
-	printf("%d\n",bsearch(lst, 0, 10, 9));
+	printf("bsearch_rec: %d\n",bsearch(lst, 0, 10, 9));
+	printf("bsearch_it: %d\n",bsearch_it(lst,0,10,9));
 }
